06d9bc08f7194d400fe03395af01f73945926e97
구태준(파트너사 (주)라드씨엔에스) <goo.taejun@eland.co.kr>
2024-01-26 08:07:51 +0900

    작업 업뎃

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,14 +1,29 @@
-import { API_URL } from '../app/(home)/page'
+import { API_URL } from "../app/(home)/page"
+import styles from "../styles/movie-info.module.css"
 
-async function getMovie(id:string) {
-	console.log(`fetching movie : ${Date.now()}`)
-	await new Promise((resolve) => setTimeout(resolve, 1000))
-	const res = await fetch(`${API_URL}/${id}`)
-	const json = res.json()
-	return json
+export async function getMovie(id: string) {
+  console.log(`fetching movie : ${Date.now()}`)
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  const res = await fetch(`${API_URL}/${id}`)
+  const json = res.json()
+  return json
 }
 
-export default async function MovieInfo({id}: {id: string}) {
-	const movie = await getMovie(id)
-	return <h6>{JSON.stringify(movie.title)}</h6>
+export default async function MovieInfo({ id }: { id: string }) {
+  const movie = await getMovie(id)
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        alt={movie.title}
+        className={styles.poster}
+      />
+      <div className={styles.info}>
+        <h1>{movie.title}</h1>
+        <h3>✨{movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+				<a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
+      </div>
+    </div>
+  )
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,14 +1,26 @@
-import { API_URL } from '../app/(home)/page'
+import { API_URL } from "../app/(home)/page"
+import styles from '../styles/movie-videos.module.css'
 
-async function getVideos(id:string){
-	console.log(`fetching videos : ${Date.now()}`)
-	await new Promise((resolve) => setTimeout(resolve, 3000))
-	const res = await fetch(`${API_URL}/${id}/videos`)
-	const json = res.json()
-	return json
+async function getVideos(id: string) {
+  console.log(`fetching videos : ${Date.now()}`)
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  const res = await fetch(`${API_URL}/${id}/videos`)
+  const json = res.json()
+  return json
 }
 
-export default async function MovieVideos({id}: {id: string}) {
-	const videos = await getVideos(id)
-	return <h6>{JSON.stringify(videos)}</h6>
+export default async function MovieVideos({ id }: { id: string }) {
+  const videos = await getVideos(id)
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allowFullScreen={true}
+        />
+      ))}
+    </div>
+  )
 }
